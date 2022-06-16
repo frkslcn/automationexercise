c6ed36dac1da3023d8126194c4cf62d2abf0976c
AyselGul <ayselyeter@hotmail.com>
2022-06-16 17:03:49 +0100

    ortak meth

--- src/test/java/stepDefinitions/US_BunyaminStepDefs.java
+++ src/test/java/stepDefinitions/US_BunyaminStepDefs.java
@@ -69,8 +69,7 @@ public class US_BunyaminStepDefs {
 
 
     @Then("Verify {string} is visible")
-    public void verifySEARCHEDPRODUCTSIsVisible() {
-
+    public void verifySEARCHEDPRODUCTSIsVisible(String str) {
 
     }
 
