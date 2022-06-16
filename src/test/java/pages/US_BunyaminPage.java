dba97b3423da91b4677dc08e15d62d71c7d0d2cd
Bunyamin <bunyaminbektas2844@gmail.com>
2022-06-16 12:12:09 +0100

    cmt

--- src/test/java/pages/US_BunyaminPage.java
+++ src/test/java/pages/US_BunyaminPage.java
@@ -10,15 +10,55 @@ public class US_BunyaminPage {
        PageFactory.initElements(Driver.getDriver(), this);
 
    }
-
-
-
    @FindBy(xpath = "(//*[.='Test Cases'])[1]")
     public WebElement testCaseButtn;
 
    @FindBy(xpath = "(//*[.='Test Cases'])[2]")
     public WebElement testCaseText;
 
+   @FindBy(xpath = "//*[@id='header']/div/div/div/div[2]/div/ul/li[2]/a")
+    public WebElement productsButton;
+
+   @FindBy (xpath = "//*[@id='sale_image']")
+    public WebElement allProductsPage;
+
+   @FindBy(xpath = "//*[@class='title text-center']")
+    public WebElement productList;
+
+   @FindBy(xpath = "(//*[.='View Product'])[1]")
+    public WebElement viewProduct;
+
+   //Automation Exercise - Product Details
+    //Automation Exercise - All Products
+  //: product name, category, price, availability, condition, brand
+
+    @FindBy(xpath = "//*[.='Brand:']")
+    public WebElement brand;
+    @FindBy(xpath = "//*[.='Condition:']")
+    public WebElement condition;
+    @FindBy(xpath = "//*[.='Availability:']")
+    public WebElement availibility;
+    @FindBy(xpath = "//*[.='Category: Women > Tops']")
+    public WebElement category;
+    @FindBy(xpath = "//*[.='Rs. 500']")
+    public WebElement price;
+    @FindBy(xpath = "//*[.='Blue Top']")
+    public WebElement name;
+
+
+    @FindBy(xpath = "//*[@id='search_product']")
+    public WebElement searchButton;
+    @FindBy(xpath = "//*[@id='submit_search']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//*[@alt='ecommerce website products']")
+    public WebElement displayedProduct01;
+
+    @FindBy(xpath = "(//*[@alt='ecommerce website products'])[7]")
+    public WebElement displayedProduct02;
+
+    @FindBy(xpath = "(//*[@alt='ecommerce website products'])[3]")
+    public WebElement displayedProduct03;
 
 
 
