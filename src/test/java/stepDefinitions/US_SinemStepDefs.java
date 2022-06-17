e4d9431bf4756590e4c16b9e31ebbbcfc6b79a81
Sinem Z <94461108+sinemZn@users.noreply.github.com>
2022-06-17 12:47:22 +0100

    005

--- src/test/java/stepDefinitions/US_SinemStepDefs.java
+++ src/test/java/stepDefinitions/US_SinemStepDefs.java
@@ -41,32 +41,13 @@ public class US_SinemStepDefs {
         Driver.wait(2);
     }
 
-//    @Then("Verify {string} is visible")
-//    public void verify_is_visible(String string) {
-//        Assert.assertTrue(us_sinemPage.loginToYourAccountText.isDisplayed());
-//        Driver.wait(2);
-//    }
     @Then("Enter correct {string} address and {string}")
     public void enter_correct_address_and(String email, String password) {
         us_sinemPage.emailLoginTextBox.sendKeys(email);
         us_sinemPage.passwordTextBox.sendKeys(password);
         Driver.wait(2);
     }
-//    @Then("Click {string} button")
-//    public void click_button(String string) {
-//        us_sinemPage.loginButton.click();
-//        Driver.wait(2);
-//    }
-//    @Then("Verify that {string} is visible")
-//    public void verify_that_is_visible(String string) {
-//        Assert.assertTrue(us_sinemPage.loggedInAs.isDisplayed());
-//        Driver.wait(2);
-//    }
-//    @Then("Click {string}")
-//    public void click(String string) {
-//        us_sinemPage.logout.click();
-//        Driver.wait(2);
-//    }
+
     @Then("Verify that user is navigated to login page")
     public void verify_that_user_is_navigated_to_login_page() {
         Assert.assertTrue(us_sinemPage.loginToYourAccountText.isDisplayed());
